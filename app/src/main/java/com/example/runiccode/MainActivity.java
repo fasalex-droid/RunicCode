16514e04e4584f6575cd344d82d01c8f4c35c228
fasav <fasav@skbkontur.ru>
2020-03-14 10:01:58 +0500

    1.04

--- app/src/main/java/com/example/runiccode/MainActivity.java
+++ app/src/main/java/com/example/runiccode/MainActivity.java
@@ -7,8 +7,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.format.DateUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
@@ -17,18 +20,33 @@ import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
+    //+++блок с датой и временем
     TextView currentDateTime;
     Calendar dateAndTime=Calendar.getInstance();
+    ImageView imageView;
+    LinearLayout linearLayout;//Для видимости области с рунами
+    boolean flag = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         currentDateTime= findViewById(R.id.currentDateTime);
         setInitialDateTime();
-
-
+        imageView = findViewById(R.id.imageView6);
+        linearLayout = findViewById(R.id.linearLayout); //Для видимости области с рунами
     }
 
+    public void onClick(View v)
+    {
+        linearLayout.setVisibility(View.VISIBLE);   //делаем видимой область с результатами рун после нажатия на кнопку "Узнать"
+
+        if (flag)
+            imageView.setImageResource(R.drawable.gebo);
+        else
+            // возвращаем первую картинку
+            imageView.setImageResource(R.drawable.hagalaz);
+        flag = !flag;
+    }
     // отображаем диалоговое окно для выбора даты
     public void setDate(View v) {
         new DatePickerDialog(MainActivity.this, d,
@@ -37,7 +55,6 @@ public class MainActivity extends AppCompatActivity {
                 dateAndTime.get(Calendar.DAY_OF_MONTH))
                 .show();
     }
-
     // отображаем диалоговое окно для выбора времени
     public void setTime(View v) {
         new TimePickerDialog(MainActivity.this, t,
@@ -47,17 +64,14 @@ public class MainActivity extends AppCompatActivity {
     }
     // установка начальных даты и времени
     private void setInitialDateTime() {
-
         @SuppressLint("SimpleDateFormat")
         SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
         String date = sdf.format(dateAndTime.getTime());
         currentDateTime.setText(date);
-
         //currentDateTime.setText(DateUtils.formatDateTime(this,
         //        dateAndTime.getTimeInMillis(),
         //        DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_SHOW_YEAR));
     }
-
     // установка обработчика выбора времени
     TimePickerDialog.OnTimeSetListener t=new TimePickerDialog.OnTimeSetListener() {
         public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
@@ -66,7 +80,6 @@ public class MainActivity extends AppCompatActivity {
             setInitialDateTime();
         }
     };
-
     // установка обработчика выбора даты
     DatePickerDialog.OnDateSetListener d=new DatePickerDialog.OnDateSetListener() {
         public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
@@ -76,4 +89,7 @@ public class MainActivity extends AppCompatActivity {
             setInitialDateTime();
         }
     };
+    //---блок с датой и временем
+
+
 }
